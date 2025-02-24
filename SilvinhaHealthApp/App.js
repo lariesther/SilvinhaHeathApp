5a85b4c01ef39657ff2ee5d04b4a0e8136ace5fe
lariesther <larissafantinatti24@gmail.com>
2025-02-24 11:43:32 -0300

    trabalhnado na pagina tela inicial

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -4,17 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.titleBox}>
+      <Text style={styles.titleText}>silvinha!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0e5e5',
+   paddingTop:50
   },
 });
