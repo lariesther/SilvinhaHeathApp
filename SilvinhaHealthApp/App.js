17bc338a083847a38e38a119627e14efe8c05385
lariesther <larissafantinatti24@gmail.com>
2025-03-06 12:28:45 -0300

    programando

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,22 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.titleBox}>
-      <Text style={styles.titleText}>silvinha!</Text>
+        <Text style={styles.titleText}>Silvinha Health App</Text>
       </View>
-      <StatusBar style="auto" />
-    </View>
-
+      <View style={styles.content}>
+        <Text style={styles.subTitle}>calculadora de IMC</Text>
+        <View>
+          <Text style={styles.label}>Altura</Text>
+           
+          </View>
+     </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e0e5e5',
-   paddingTop:50
+    backgroundColor:'#edf2f4',
   },
+  titleBox: {
+    alignItems:'center',
+    justifyContent: 'flex-end',
+    height: 100,
+    backgroundColor: '#ef233c',
+    borderBottomStartRadius: 50,
+    borderBottomEndRadius: 50,
+  },
+  titleText:
+  {
+    color: '#edf2f4',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  content:{
+    flex: 1,
+    padding: 40,
+    width:'100%',
+    backgroundColor: '#edf2f4',
+  },
+  subTitle: {
+    textAlign:'center',
+    fontSize: 24,
+    color: '#ef233c',
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+
 });
