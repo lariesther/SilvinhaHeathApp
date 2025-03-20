5e486e989183aae34e14ff3cf60b3709b7cac1af
lariesther <larissafantinatti24@gmail.com>
2025-03-20 12:30:03 -0300

    programando

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,13 +1,34 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, TextInput, Touchable, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Touchable, TouchableOpacity, Keyboard } from 'react-native';
 import Ionicons from "@expo/vector-icons/Ionicons"
 export default function App() {
-  const [height, setHeight] =useState(null);
-  const [weight, setweight] =useState(null);
-  const [imc, setImc] =useState(null);
-  const [texteButton,setTextButton] =useState("Calcular");
-  const [messageImc, setMessageImc]=useState("Preencha o peso e a altura")
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [imc, setImc] = useState(null);
+  const [texteButton, setTextButton] = useState("Calcular");
+  const [messageImc, setMessageImc] = useState("Preencha o peso e a altura")
+  
+  function imcCalculator() {
+    // (peso /(altura* altura))
+    setImc((weight / (height * height)).toFixed(2));
+  }
+  function validateImc() {
+    if (weight != null && height != null) {
+      Keyboard.dismiss();
+      imcCalculator();
+      setHeight(null);
+      setWeight(null);
+      setTextButton("Calcular Novamente");
+      setMessageImc("Seu IMC é igual a:");
+      return;
+    }
+
+    setImc(null);
+    setTextButton("Calcular");
+    setMessageImc("preencha peso e altura");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.titleBox}>
@@ -18,34 +39,39 @@ export default function App() {
         <Text style={styles.subTitle}>calculadora de IMC</Text>
         <View>
           <Text style={styles.label}>Altura</Text>
-           <TextInput
-           style={ styles.input}
-           placeholder='Ex. 1.70'
-           keyboardType='numeric'
-           />
-           </View>
-           <View style={{Margintop: 25 }}>
-        <Text style={styles.label}>Peso</Text>
-           <TextInput
-           style={ styles.input}
-           placeholder='Ex. 80.00'
-           keyboardType='numeric'
-           />
-           
-          </View>
-          <TouchableOpacity style={styles.buttom}
-          onPress={ () => alert('SEGREDO')}
-          >
-            <Ionicons name={"calculator-sharp"} size={22} color='#edf2f4' />
-            <Text style={styles.text}> Calcular</Text>
-          </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            onChangeText={setHeight}
+            value={height ?? ''}
+            placeholder='Ex. 1.70'
+            KeyboardType='numeric'
+          />
+        </View>
+        <View style={{ Margintop: 25 }}>
+          <Text style={styles.label}>Peso</Text>
+          <TextInput
+            style={styles.input}
+            onChange={setWeight}
+            value={weight ?? ''}
+            placeholder='Ex. 80.00'
+            KeyboardType='numeric'
+          />
 
-          <View style={styles.imcConteiner}>
-            <Text style={styles.imcText}>preencha o peso e a altura</Text>
-            <Text style={styles.imcResult}>666</Text>
-          </View>
-          
-     </View>
+        </View>
+        <TouchableOpacity style={styles.buttom}
+          onPress={() => validateImc()}
+        >
+
+          <Ionicons name={"calculator-sharp"} size={24} color='#edf2f4' />
+          <Text style={styles.text}>{texteButton}</Text>
+        </TouchableOpacity>
+
+        <View style={styles.imcConteiner}>
+          <Text style={styles.imcText}>{messageImc}</Text>
+          <Text style={styles.imcResult}>{imc}</Text>
+        </View>
+
+      </View>
       <StatusBar style="light" />
     </SafeAreaView>
   );
@@ -54,10 +80,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#edf2f4',
+    backgroundColor: '#edf2f4',
   },
   titleBox: {
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'flex-end',
     height: 100,
     backgroundColor: '#0080d0',
@@ -71,71 +97,71 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  content:{
+  content: {
     flex: 1,
     padding: 40,
-    width:'100%',
+    width: '100%',
     backgroundColor: '#edf2f4',
   },
   subTitle: {
-    textAlign:'center',
+    textAlign: 'center',
     fontSize: 24,
     color: '#0080d0',
     fontWeight: 'bold',
     marginBottom: 40,
   },
-  label:{
-    color:'#000',
+  label: {
+    color: '#000',
     fontSize: 18,
 
   },
-  input:{
+  input: {
     height: 45,
-    width:'100%',
+    width: '100%',
     paddingHorizontal: 10,
     fontSize: 18,
-    borderColor:'#0080d0',
+    borderColor: '#0080d0',
     borderBottomWidth: 3,
     marginVertical: 5,
-   
+
 
   },
-  buttom:{
-    height:55,
-    width:"100%",
-    paddingVertical:15,
-    flexDirection:"row",
-    justifyContent:"center",
-    alignItems:"center",
-    backgroundColor:"#0080d0",
-    borderRadius:15,
-    marginTop:40,
-    margimBottom:10,
+  buttom: {
+    height: 55,
+    width: "100%",
+    paddingVertical: 15,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0080d0",
+    borderRadius: 15,
+    marginTop: 40,
+    margimBottom: 10,
   },
 
-  text:{ 
-    color:"#edf2f4",
-    fontSize:20,
-    fontWeight:"bold",
-    marginLeft:5
+  text: {
+    color: "#edf2f4",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 5
 
   },
   imcConteiner: {
     flex: 1,
-    alignItems:"center",
-    justifyContent:"center",
-    width:"100%"
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%"
   },
   imcText: {
-    fontSize:18,
+    fontSize: 18,
     color: "#0080d0",
-    fontWeight:"bold"
+    fontWeight: "bold"
   },
 
   imcResult: {
-fontSize:48,
-color:'#0080d0',
-fontWeight:'bold'
+    fontSize: 48,
+    color: '#0080d0',
+    fontWeight: 'bold'
   }
 
 });
